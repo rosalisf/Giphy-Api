d10b8b1cd1aed81fd7267524ca17da70488c70c6
Rosalis Freebairn <rfreeb@artic.edu>
2019-09-02 20:49:38 -0500

    COnnected to bootstrap and jquery libraries. Basic skeleton in html and js files.

---
+++ script.js
@@ -0,0 +1,30 @@
+$("button").on("click", function() {
+  const person = $(this).attr("data-person");
+  const queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    person +
+    "&api_key=E1zsyf6xVP2cq9m4VOOms0qR9GRNd9qH";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    const results = response.data;
+
+    for (let i = 0; i < results.length; i++) {
+      let gifDiv = $("<div>");
+
+      const rating = results[i].rating;
+
+      const p = $("<p>").text("Rating: " + rating);
+
+      let personImage = $("<img>");
+      personImage.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(personImage);
+
+      $("#gif-section").prepend(gifDiv);
+    }
+  });
+});
